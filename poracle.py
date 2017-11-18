c8b85fb3ccdcb1e6fc55517d37e7f5f564ee2fe5
Derek Mäekask <derek.maekask@myjar.com>
2017-11-18 11:04:10 +0200

    server receives stuff

---
+++ poracle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import logging
+import os
+from utils.args import args
+from gevent import wsgi, spawn
+from utils import config
+from flask import Flask, request, abort
+import Queue
+import json
+
+app = Flask(__name__)
+data_queue = Queue.Queue()
+
+# create logger
+log = logging.getLogger('Poracle')
+log.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s [%(threadName)18s][%(module)14s]' +
+                              '[%(levelname)8s] %(message)s')
+ch.setFormatter(formatter)
+log.addHandler(ch)
+args(os.path.abspath(os.path.dirname(__file__)))
+
+def potato():
+    log.info("Poracle is running on: http://{}:{}".format(config['HOST'], config['PORT']))
+    server = wsgi.WSGIServer((config['HOST'], config['PORT']), app, log=logging.getLogger('Webserver'))
+    server.serve_forever()
+
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return "Schwing!"
+
+@app.route('/', methods=['POST'])
+def accept_webhook():
+    try:
+        log.debug("{} Sent me something.".format(request.remote_addr))
+        data = json.loads(request.data)
+        if type(data) == dict: # older webhook style
+            log.debug(data)
+        else:   # For RM's frame
+            for frame in data:
+                log.debug(data)
+    except Exception as e:
+        log.error("I am unhappy, computer says: {}: {}".format(type(e).__name__, e))
+        abort(400)
+    return "OK"  # request ok
+
+if __name__ == '__main__':
+    log.info("Poracle initializing.")
+    potato()
---
+++ utils/args.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import configargparse
+import os
+import sys
+from . import config
+
+
+
+def args(root_path):
+
+    config['ROOT_PATH'] = root_path
+    config_files = [get_path('../config/config.ini')] if '-cf' not in sys.argv and '--config' not in sys.argv else []
+    parser = configargparse.ArgParser(description='Poracle', default_config_files=config_files)
+    parser.add_argument('-cf', '--config', is_config_file=True, help='Configuration file')
+    parser.add_argument('-d', '--debug', help='Debug Mode', action='store_true', default=False)
+    parser.add_argument('-H', '--host', help='Set web server listening host', default='127.0.0.1')
+    parser.add_argument('-P', '--port', type=int, help='Web server port', default=3030)
+
+    args = parser.parse_args()
+
+    config['HOST'] = args.host
+    config['PORT'] = args.port
+    config['DEBUG'] = args.debug
+
+    return parser.parse_args()
+
+
+def get_path(path):
+    if not os.path.isabs(path):  # If not absolute path
+        path = os.path.join(config['ROOT_PATH'], path)
+    return path
